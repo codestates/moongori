3e267443e10a31c0f3bda9a8414ad7e83a94b8b5
이승준 <lsj135779@gmail.com>
2021-11-26 14:13:14 +0900

    client:App:add:router-divide

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,56 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./pages/Main";
+
+// import {
+//   Main,
+//   TradeList,
+//   NewsList,
+//   Mypage,
+//   TradeNormalPost,
+//   TradeSuggestionPost,
+//   TradePostWrite,
+//   NewsPost,
+//   NewsPostWrite,
+//   Chat,
+// } from "pages";
 
 function App() {
   return (
-    <div>
-      Hello world!!!!
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+
+          {/* <Route path="/trade=:category" element={<TradeList />} />
+
+          <Route path="/news=:category" element={<NewsList />} />
+
+          <Route path="/mypage" element={<Mypage />} />
+
+          <Route path="/trade-normal/read=:id" element={<TradeNormalPost />} />
+
+          <Route
+            path="/trade-suggestion/read=:id"
+            element={<TradeSuggestionPost />}
+          />
+
+          <Route path="/trade/write" element={<TradePostWrite />} />
+
+          <Route path="/news/read=:id" element={<NewsPost />} />
+
+          <Route path="/news/write" element={<NewsPostWrite />} />
+
+          <Route path="/chat" element={<Chat />} /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import logoImg from "./../images/문고리 로고.png";
+
+const StHaedBoxDiv = styled.div``;
+
+export default function Header() {
+  return <StHaedBoxDiv>헤더영역</StHaedBoxDiv>;
+}
