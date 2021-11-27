ef5a2db427a100dc6d2242e0baece289f5e1f3e8
Leeseongmin <lsm6627@gmail.com>
2021-11-27 17:26:19 +0900

    index 수정

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
+  res.status(200).send("Hello World!");
 });
 
 module.exports = app.listen(PORT, () => {
-    console.log(`Server On: http://localhost:${PORT}/`)
-})
+  console.log(`this server listening on: ${PORT}/`);
+});
