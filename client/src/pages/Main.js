f4b2b3eeb75446f1c0b33a7decf4eb0c14bcbce8
Leeseongmin <lsm6627@gmail.com>
2021-11-29 14:24:50 +0900

    client Main login-test

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,103 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import queryString from "query-string";
+import kakaoImg from "../images/kakao_login_medium_narrow.png";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 export default function Main() {
-	return (
-		<>
-		{/* <div >{`메인페이지none | (en-US) <transform-list>
-	where 
-	<transform-list> = <transform-function>+ (en-US)
-
-	where 
-	<transform-function> = <matrix()> | (en-US) <translate()> | (en-US) <translateX()> | (en-US) <translateY()> | (en-US) <scale()> | (en-US) <scaleX()> | (en-US) <scaleY()> | (en-US) <rotate()> | (en-US) <skew()> | (en-US) <skewX()> | (en-US) <skewY()> | (en-US) <matrix3d()> | (en-US) <translate3d()> | (en-US) <translateZ()> | (en-US) <scale3d()> | (en-US) <scaleZ()> | (en-US) <rotate3d()> | (en-US) <rotateX()> | (en-US) <rotateY()> | (en-US) <rotateZ()> | (en-US) <perspective()>
-
-	where 
-	<matrix()> = matrix( <number># (en-US){ (en-US)6} (en-US) )
-	<translate()> = translate( <length-percentage> , <length-percentage>? (en-US) )
-	<translateX()> = translateX( <length-percentage> )
-	<translateY()> = translateY( <length-percentage> )
-	<scale()> = scale( <number> , <number>? (en-US) )
-	<scaleX()> = scaleX( <number> )
-	<scaleY()> = scaleY( <number> )
-	<rotate()> = rotate( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<skew()> = skew( [ (en-US) <angle> | (en-US) <zero> ] (en-US) , [ (en-US) <angle> | (en-US) <zero> ] (en-US)? (en-US) )
-	<skewX()> = skewX( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<skewY()> = skewY( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<matrix3d()> = matrix3d( <number># (en-US){ (en-US)16} (en-US) )
-	<translate3d()> = translate3d( <length-percentage> , <length-percentage> , <length> )
-	<translateZ()> = translateZ( <length> )
-	<scale3d()> = scale3d( <number> , <number> , <number> )
-	<scaleZ()> = scaleZ( <number> )
-	<rotate3d()> = rotate3d( <number> , <number> , <number> , [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateX()> = rotateX( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateY()> = rotateY( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateZ()> = rotateZ( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<perspective()> = perspective( <length> )
-
-	where 
-	<length-percentage> = <length> | (en-US) <percentage>none | (en-US) <transform-list>
-	where 
-	<transform-list> = <transform-function>+ (en-US)
-
-	where 
-	<transform-function> = <matrix()> | (en-US) <translate()> | (en-US) <translateX()> | (en-US) <translateY()> | (en-US) <scale()> | (en-US) <scaleX()> | (en-US) <scaleY()> | (en-US) <rotate()> | (en-US) <skew()> | (en-US) <skewX()> | (en-US) <skewY()> | (en-US) <matrix3d()> | (en-US) <translate3d()> | (en-US) <translateZ()> | (en-US) <scale3d()> | (en-US) <scaleZ()> | (en-US) <rotate3d()> | (en-US) <rotateX()> | (en-US) <rotateY()> | (en-US) <rotateZ()> | (en-US) <perspective()>
-
-	where 
-	<matrix()> = matrix( <number># (en-US){ (en-US)6} (en-US) )
-	<translate()> = translate( <length-percentage> , <length-percentage>? (en-US) )
-	<translateX()> = translateX( <length-percentage> )
-	<translateY()> = translateY( <length-percentage> )
-	<scale()> = scale( <number> , <number>? (en-US) )
-	<scaleX()> = scaleX( <number> )
-	<scaleY()> = scaleY( <number> )
-	<rotate()> = rotate( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<skew()> = skew( [ (en-US) <angle> | (en-US) <zero> ] (en-US) , [ (en-US) <angle> | (en-US) <zero> ] (en-US)? (en-US) )
-	<skewX()> = skewX( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<skewY()> = skewY( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<matrix3d()> = matrix3d( <number># (en-US){ (en-US)16} (en-US) )
-	<translate3d()> = translate3d( <length-percentage> , <length-percentage> , <length> )
-	<translateZ()> = translateZ( <length> )
-	<scale3d()> = scale3d( <number> , <number> , <number> )
-	<scaleZ()> = scaleZ( <number> )
-	<rotate3d()> = rotate3d( <number> , <number> , <number> , [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateX()> = rotateX( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateY()> = rotateY( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateZ()> = rotateZ( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<perspective()> = perspective( <length> )
-
-	where 
-	<length-percentage> = <length> | (en-US) <percentage>none | (en-US) <transform-list>
-	where 
-	<transform-list> = <transform-function>+ (en-US)
-
-	where 
-	<transform-function> = <matrix()> | (en-US) <translate()> | (en-US) <translateX()> | (en-US) <translateY()> | (en-US) <scale()> | (en-US) <scaleX()> | (en-US) <scaleY()> | (en-US) <rotate()> | (en-US) <skew()> | (en-US) <skewX()> | (en-US) <skewY()> | (en-US) <matrix3d()> | (en-US) <translate3d()> | (en-US) <translateZ()> | (en-US) <scale3d()> | (en-US) <scaleZ()> | (en-US) <rotate3d()> | (en-US) <rotateX()> | (en-US) <rotateY()> | (en-US) <rotateZ()> | (en-US) <perspective()>
-
-	where 
-	<matrix()> = matrix( <number># (en-US){ (en-US)6} (en-US) )
-	<translate()> = translate( <length-percentage> , <length-percentage>? (en-US) )
-	<translateX()> = translateX( <length-percentage> )
-	<translateY()> = translateY( <length-percentage> )
-	<scale()> = scale( <number> , <number>? (en-US) )
-	<scaleX()> = scaleX( <number> )
-	<scaleY()> = scaleY( <number> )
-	<rotate()> = rotate( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<skew()> = skew( [ (en-US) <angle> | (en-US) <zero> ] (en-US) , [ (en-US) <angle> | (en-US) <zero> ] (en-US)? (en-US) )
-	<skewX()> = skewX( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<skewY()> = skewY( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<matrix3d()> = matrix3d( <number># (en-US){ (en-US)16} (en-US) )
-	<translate3d()> = translate3d( <length-percentage> , <length-percentage> , <length> )
-	<translateZ()> = translateZ( <length> )
-	<scale3d()> = scale3d( <number> , <number> , <number> )
-	<scaleZ()> = scaleZ( <number> )
-	<rotate3d()> = rotate3d( <number> , <number> , <number> , [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateX()> = rotateX( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateY()> = rotateY( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<rotateZ()> = rotateZ( [ (en-US) <angle> | (en-US) <zero> ] (en-US) )
-	<perspective()> = perspective( <length> )
-
-	where 
-	<length-percentage> = <length> | (en-US) <percentage>`}</div>; */}
-		</>
-	)
+  const handleKakaoLoginBtn = async () => {
+    await window.location.assign("http://localhost:80/user/kakao");
+  };
+  useEffect(() => {
+    const query = queryString.parse(window.location.search);
+    if (query.access_token) {
+      axios.get("http://localhost:80/user/info").then((res) => {
+        console.log(res.data.data);
+      });
+    }
+  }, [window.location]);
+  return (
+    <>
+      <div
+        onClick={() => {
+          handleKakaoLoginBtn();
+        }}
+      >
+        <img src={kakaoImg} />
+      </div>
+	  
+      {/* <img
+        src={`http://k.kakaocdn.net/dn/ec5JId/btqBDYf4uEv/4n4rER4879ROYlsfVit82K/img_640x640.jpg`}
+      /> */}
+      
+    </>
+  );
 }
