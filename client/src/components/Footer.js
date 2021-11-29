56ced481b0797504adf417108882b2c2abf28cc5
rmfhsep <rmfhsep@gmail.com>
2021-11-29 13:37:27 +0900

    [client] add:add footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,6 +4,8 @@ import styled, { css } from "styled-components";
 import logoWhite from "./../images/흰색 로고.png";
 
 const FooterBox = styled.div`
+  background-color: #777777;
+  color: #ffffff;
   position: absolute;
   bottom: 0px;
   border: 1px solid;
@@ -11,15 +13,15 @@ const FooterBox = styled.div`
   flex-direction: column;
   align-items: center;
   height: 15%;
-  width: 90%;
+
   // phone
-  @media all and (max-width: 360px) {
+  @media all and (max-width: 767px) {
     border: 1px solid;
     margin: 0 5% 0 5%;
+    width: 90%;
   }
   .LogoName {
     margin-top: 3px;
-    color: black;
     text-decoration: none;
     font-family: Fredoka One;
   }
@@ -31,13 +33,18 @@ const FooterBox = styled.div`
     height: 20px;
   }
   .footer-master {
-    width: 80%;
+    width: 100%;
+    height: 25%;
+    display: flex;
+    justify-content: center;
   }
   .footer-material {
-    border-bottom: 1px solid gray;
+    width: 40%;
+    border-bottom: 1px solid #8f8f8f;
     flex-direction: row;
     display: flex;
     justify-content: center;
+    align-items: center;
     text-align: center;
   }
   .Footer-content {
@@ -48,14 +55,14 @@ const FooterBox = styled.div`
     justify-content: center;
     text-align: center;
   }
-  .About-title {
+  .About-wrap {
+    color: #ffffff;
     font-size: 9px;
     font-weight: bold;
     margin-bottom: 2px;
   }
   .About-content {
     font-size: 7px;
-    color: gray;
     font-weight: normal;
   }
   .Contact-title {
@@ -65,50 +72,142 @@ const FooterBox = styled.div`
     margin-bottom: 2px;
   }
   .Contact-content {
-    color: gray;
     font-size: 7px;
     font-weight: normal;
   }
   .Copyright {
-    height: 5%;
-    color: gray;
+    margin-bottom: 10px;
+    width: 100%;
+    height: 15%;
     font-size: 5px;
     font-weight: normal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .footer-wrap {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    bottom: 0px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
   // web
-  @media all and (min-width: 361px) {
+  @media all and (min-width: 768px) {
     bottom: 0px;
-    height: 10%;
+    height: 20%;
+    margin: 0 15% 0 15%;
+    width: 70%;
+  }
+  .footer-master {
+    width: 100%;
+    height: 20%;
+    display: flex;
+    justify-content: center;
+  }
+  .footer-material {
+    width: 60%;
+    border-bottom: 1px solid #8f8f8f;
+    flex-direction: row;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  .Footer-content {
+    width: 100%;
+    height: 60%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    text-align: center;
+  }
+  .About-wrap {
+    margin-top: 20px;
+    width: 50%;
+    font-size: 9px;
+    font-weight: bold;
+    margin-bottom: 2px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    text-align: start;
+  }
+
+  .content-wrap {
+    width: 60%;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .About-us {
+    width: 50%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .About-content {
+    margin-top: 10px;
+  }
+  .Contact-us {
+    width: 50%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .Contact-wrap {
+    text-align: start;
+    margin-top: 20px;
+    width: 60%;
+    font-size: 9px;
+    font-weight: bold;
+    margin-bottom: 2px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    text-align: start;
+  }
+  .Contact-content {
+    margin-top: 10px;
   }
 `;
 
 export default function Footer() {
   return (
     <FooterBox>
-      <div className={"footer-master"}>
-        <div className={"footer-material"}>
-          <img src={logoWhite}></img>
-          <div className={"LogoName"}>moongori</div>
-        </div>
-      </div>
-      <div className={"Footer-content"}>
-        <div className={"About-us"}>
-          <div className={"About-title"}>
-            About us
-            <div className={"About-content"}>Moongori Wiki</div>
-            <div className={"About-content"}>Repository</div>
+      <div className={"footer-wrap"}>
+        <div className={"footer-master"}>
+          <div className={"footer-material"}>
+            <img src={logoWhite}></img>
+            <div className={"LogoName"}>moongori</div>
           </div>
         </div>
-        <div className={"Contact-us"}>
-          <div className={"Contact-title"}>
-            Contact us
-            <div className={"Contact-content"}></div>
-            <div className={"Contact-content"}>사범기 김정훈 이성민 이승준</div>
+        <div className={"Footer-content"}>
+          <div className={"content-wrap"}>
+            <div className={"About-us"}>
+              <div className={"About-wrap"}>
+                <div className={"About-title"}>About us</div>
+                <div className={"About-content"}>
+                  <div>Moongori Wiki</div>
+                  <div>Repository</div>
+                </div>
+              </div>
+            </div>
+            <div className={"Contact-us"}>
+              <div className={"Contact-wrap"}>
+                <div className={"Contact-comment"}>Contact us</div>
+                <div className={"Contact-content"}>
+                  사범기 김정훈 이성민 이승준
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={"Copyright"}>
-        <div>Copyright The Moon project Team</div>
+        <div className={"Copyright"}>
+          <div>Copyright The Moon project Team</div>
+        </div>
       </div>
     </FooterBox>
   );
