5d08741a97d682d91312c52eb684c00f9f38749b
이승준 <lsj135779@gmail.com>
2021-12-06 17:06:57 +0900

    [client] add,fix:make newsPost-page and fix width size

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import NewsList from "./pages/NewsList";
 import Mypage from "./pages/Mypage";
 import NotFound from "./pages/NotFound";
 import PrivateRoute from "./components/PrivateRoute";
-
+import NewsPost from "./pages/NewsPost";
 import axios from "axios";
 
 import Swal from "sweetalert2";
@@ -43,12 +43,9 @@ export default function App() {
 
   const handleLogout = () => {
     axios.post(`${process.env.REACT_APP_API_URL}/user/signout`).then(() => {
-
       window.location.href = "/";
       isLogin(false);
       setUserinfo(null);
-
-
     });
   };
 
@@ -78,25 +75,26 @@ export default function App() {
             path="/mypage"
             element={<PrivateRoute login={login} userinfo={userinfo} />}
           />
-
-          {/* <Route path="/trade-normal/read=:id" element={<TradeNormalPost />} />
+          {/* 
+          <Route path="/trade-normal/read=:id" element={<TradeNormalPost />} />
 
           <Route
             path="/trade-suggestion/read=:id"
             element={<TradeSuggestionPost />}
           />
 
-          <Route path="/trade/write" element={<TradePostWrite />} />
+          <Route path="/trade/write" element={<TradePostWrite />} /> */}
 
-          <Route path="/news/read=:id" element={<NewsPost />} />
+          <Route
+            path="/news/read=:id"
+            element={<NewsPost login={login} userinfo={userinfo} />}
+          />
 
-          <Route path="/news/write" element={<NewsPostWrite />} />
+          {/* <Route path="/news/write" element={<NewsPostWrite />} /> */}
 
-          <Route path="/chat" element={<Chat />} /> */}
-
+          {/* <Route path="/chat" element={<Chat />} /> */}
 
           <Route element={<NotFound />} />
-
         </Routes>
         <Footer />
       </Wrap>
